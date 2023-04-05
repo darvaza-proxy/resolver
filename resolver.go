c8c68faca4ecadf2d9982be87c8b174aaf40270d
Alejandro Mery <amery@jpi.io>
2023-04-05 22:41:32 +0000

    resolver: introduce ZeroLookuper, always failing
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- resolver.go
+++ resolver.go
@@ -1,2 +1,25 @@
 // Package resolver provides DNS lookup functions
 package resolver
+
+import (
+	"context"
+	"errors"
+
+	"github.com/miekg/dns"
+)
+
+var (
+	_ Lookuper = (*ZeroLookuper)(nil)
+)
+
+var (
+	errNoAnswer = errors.New("no answer")
+)
+
+// A ZeroLookuper is a Lookuper that never finds anything
+type ZeroLookuper struct{}
+
+// Lookup implements Lookuper but always fails
+func (ZeroLookuper) Lookup(_ context.Context, _ string, _ uint16) (*dns.Msg, error) {
+	return nil, errNoAnswer
+}
