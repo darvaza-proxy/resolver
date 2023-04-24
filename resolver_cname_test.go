3510363b7bc20b38dfb6c72a92b7c18dc06ac7cb
Alejandro Mery <amery@jpi.io>
2023-04-24 17:07:06 +0000

    LookupCNAME: add test by karasz
    https://github.com/darvaza-proxy/resolver/pull/17#pullrequestreview-1398404313
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ resolver_cname_test.go
@@ -0,0 +1,45 @@
+package resolver
+
+import (
+	"context"
+	"strings"
+	"testing"
+)
+
+func TestLookupCNAME(t *testing.T) {
+	tests := []struct {
+		name   string
+		cnames []string
+	}{
+		{"www.iana.org", []string{"icann.org", "ianawww.vip.icann.org"}},
+		{"cname-to-txt.go4.org", []string{"test-txt-record.go4.org"}},
+	}
+	ctx := context.Background()
+	h, err := NewRootLookuper("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	l := NewResolver(h)
+	for _, tc := range tests {
+		s, err := l.LookupCNAME(ctx, tc.name)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		if cnameInList(s, tc.cnames...) {
+			t.Logf("%q CNAME %q", tc.name, s)
+		} else {
+			t.Errorf("%q CNAME %q, expected %q", tc.name, s, tc.cnames)
+		}
+	}
+}
+
+func cnameInList(value string, options ...string) bool {
+	value = Decanonize(strings.ToLower(value))
+	for _, opt := range options {
+		if value == opt {
+			return true
+		}
+	}
+	return false
+}
