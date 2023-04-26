a3386f2ed1576712488cc0a674c8d4d9b6f61ce3
Alejandro Mery <amery@jpi.io>
2023-04-26 11:39:21 +0000

    LookupIP: refactor test
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- resolver_ip_test.go
+++ resolver_ip_test.go
@@ -2,38 +2,79 @@ package resolver
 
 import (
 	"context"
+	"net/netip"
 	"testing"
 )
 
-//revive:disable
 func TestLookupNetIP(t *testing.T) {
 	tests := []struct {
 		name    string
 		network string
-		wantok  bool
 	}{
-		{name: "IP", network: "ip", wantok: false},
-		{name: "IPv4", network: "ip4", wantok: true},
-		{name: "IPv6", network: "ip6", wantok: true},
+		{name: "google.com", network: "ip"},
+		{name: "google.com", network: "ip4"},
+		{name: "google.com", network: "ip6"},
 	}
+
+	ctx := context.Background()
 	l := NewResolver(NewCloudflareLookuper())
 	for _, tc := range tests {
-		s, err := l.LookupNetIP(context.Background(), tc.network, "google.com")
-		if err != nil {
-			t.Fatalf(err.Error())
+		s, err := l.LookupNetIP(ctx, tc.network, tc.name)
+		switch {
+		case err != nil:
+			t.Errorf("%q %q failed: %s", tc.name, tc.network, err)
+		case checkTestLookupNetIPResponse(t, tc.name, tc.network, s):
+			t.Logf("%q %q: %q", tc.name, tc.network, s)
 		}
-		t.Log(s)
-		if tc.wantok {
-			if tc.network == "ip4" {
-				if !s[0].Is4() {
-					t.Fatalf("called for IPv4, received something else")
-				}
-			}
-			if tc.network == "ip6" {
-				if !s[0].Is6() {
-					t.Fatalf("called for IPv6, received something else")
-				}
-			}
+	}
+}
+
+func checkTestLookupNetIPResponse(t *testing.T,
+	name, network string,
+	result []netip.Addr) bool {
+	//
+	var ip4, ip6, invalid bool
+	var msg string
+
+	for _, addr := range result {
+		switch {
+		case !addr.IsValid():
+			invalid = true
+		case addr.Is4():
+			ip4 = true
+		case addr.Is6():
+			ip6 = true
+		default:
+			invalid = true
 		}
+	}
+
+	msg, ok := validateTestLookupNetIPResult(network, invalid, ip4, ip6)
+	if !ok {
+		t.Errorf("%q %q: %s: %q", name, network, msg, result)
+	}
+	return ok
+}
+
+// revive:disable:cyclomatic
+func validateTestLookupNetIPResult(network string,
+	invalid, ip4, ip6 bool) (string, bool) {
+	// revive:enable:cyclomatic
+	want4 := network == "ip" || network == "ip4"
+	want6 := network == "ip" || network == "ip6"
+
+	switch {
+	case invalid:
+		return "invalid address", false
+	case ip4 && !want4:
+		return "unexpected IPv4 address", false
+	case ip6 && !want6:
+		return "unexpected IPv6 address", false
+	case !ip4 && want4:
+		return "missing IPv4 address", false
+	case !ip6 && want6:
+		return "missing IPv6 address", false
+	default:
+		return "", true
 	}
 }
