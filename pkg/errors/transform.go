eaa2280ea98ecc0b7c620379551effdade3ba43b
Alejandro Mery <amery@jpi.io>
2023-12-03 22:32:25 +0000

    errors: introduce ErrorAsMsg()
    introduce ErrorAsMsg() to convert Lookup errors into [dns.Msg]
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/errors/transform.go
+++ pkg/errors/transform.go
@@ -64,6 +64,71 @@ func MsgAsError(r *dns.Msg) *net.DNSError {
 	}
 }
 
+// ErrorAsMsg attempts to convert a [net.DNSError] into a [dns.Msg] response
+func ErrorAsMsg(req *dns.Msg, err error) *dns.Msg {
+	if err == nil {
+		// no error
+		if req == nil {
+			return nil
+		}
+
+		// good reply
+		return newResponseSuccess(req)
+	}
+
+	if e, ok := err.(*net.DNSError); ok {
+		// net.DNSError
+		return dnsErrorAsMsg(req, e)
+	}
+
+	// any other error
+	return newResponseServerFailure(req)
+}
+
+func dnsErrorAsMsg(req *dns.Msg, err *net.DNSError) *dns.Msg {
+	switch err.Err {
+	case NOANSWER:
+	case NOTYPE:
+		resp := newResponseSuccess(req)
+		resp.Authoritative = true
+		return resp
+	case NXDOMAIN:
+		return newResponseRcode(req, dns.RcodeNameError)
+	case TRUNCATED:
+	case BADREQUEST:
+	case BADRESPONSE:
+	case NOTIMPLEMENTED:
+		return newResponseRcode(req, dns.RcodeNotImplemented)
+	default:
+		rcode, ok := dns.StringToRcode[err.Err]
+		if ok {
+			return newResponseRcode(req, rcode)
+		}
+	}
+
+	// any other error
+	return newResponseServerFailure(req)
+}
+
+func newResponseRcode(req *dns.Msg, rcode int) *dns.Msg {
+	if req == nil {
+		// we can't SetRcode with a nil request
+		req = new(dns.Msg)
+	}
+
+	resp := new(dns.Msg)
+	resp.SetRcode(req, rcode)
+	return resp
+}
+
+func newResponseSuccess(req *dns.Msg) *dns.Msg {
+	return newResponseRcode(req, dns.RcodeSuccess)
+}
+
+func newResponseServerFailure(req *dns.Msg) *dns.Msg {
+	return newResponseRcode(req, dns.RcodeServerFailure)
+}
+
 // ValidateResponse analyses a [dns.Exchange] response and
 // produces a matching [net.DNSError] if it's an error
 func ValidateResponse(server string, r *dns.Msg, err error) *net.DNSError {
