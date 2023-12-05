5bcfa8249e77e71ac87bfa5c5acf57ae4fe16b1c
Alejandro Mery <amery@jpi.io>
2023-12-05 21:36:45 +0000

    client: introduce Client interface, matching our use of [dns.Client]
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ pkg/client/client.go
@@ -0,0 +1,24 @@
+// Package client implements DNS client wrappers
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+var (
+	_ Client = (*dns.Client)(nil)
+)
+
+// A Client makes a request to a server
+type Client interface {
+	ExchangeContext(context.Context, *dns.Msg, string) (*dns.Msg, time.Duration, error)
+}
+
+// NewDefaultClient allocate a default [dns.Client] in the same
+// manner as dns.ExchangeContext(), plain UDP.
+func NewDefaultClient() Client {
+	return &dns.Client{Net: "udp"}
+}
