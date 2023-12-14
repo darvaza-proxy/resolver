e23c871240d2ad7558b45cee868943a931b22359
Alejandro Mery <amery@jpi.io>
2023-12-14 19:18:25 +0000

    common: introduce TrimRR() and TrimQ() helpers
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ pkg/common/msg.go
@@ -0,0 +1,36 @@
+// Package common contains helpers to work with [dns.Msg]
+package common
+
+import "github.com/miekg/dns"
+
+// TrimQ removes entries matching the condition from a dns.Question slice
+func TrimQ(s []dns.Question, cond func(q dns.Question) bool) []dns.Question {
+	var j int
+
+	for i, q := range s {
+		if !cond(q) {
+			if i != j {
+				s[j] = q
+			}
+			j++
+		}
+	}
+
+	return s[:j]
+}
+
+// TrimRR removes entries matching the condition from a dns.RR slice
+func TrimRR(s []dns.RR, cond func(rr dns.RR) bool) []dns.RR {
+	var j int
+
+	for i, rr := range s {
+		if !cond(rr) {
+			if i != j {
+				s[j] = rr
+			}
+			j++
+		}
+	}
+
+	return s[:j]
+}
